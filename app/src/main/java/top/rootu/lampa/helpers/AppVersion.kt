673d4914df3fa47732fd2e0bba1ea1d18542cb56
ARST113 <96774211+ARST113@users.noreply.github.com>
2026-09-25 23:20:08 +0300

    Handle nullable PackageInfo.versionName with compileSdk 36

--- app/src/main/java/top/rootu/lampa/helpers/AppVersion.kt
+++ app/src/main/java/top/rootu/lampa/helpers/AppVersion.kt
@@ -22,7 +22,7 @@ fun getAppVersion(
             packageManager.getPackageInfo(packageName, 0)
         }
         AppVersion(
-            versionName = packageInfo.versionName,
+            versionName = packageInfo.versionName ?: "unknown",   // compileSdk 36 помечает versionName как nullable
             versionNumber = PackageInfoCompat.getLongVersionCode(packageInfo),
         )
     } catch (_: Exception) {
