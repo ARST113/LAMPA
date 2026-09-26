0f72e0058c86cae8ee83d568114f47bf777a4681
ARST113 <96774211+ARST113@users.noreply.github.com>
2026-09-26 15:50:02 +0300

    Remove obsolete Crosswalk JavaScript annotations

--- app/src/main/java/top/rootu/lampa/AndroidJS.kt
+++ app/src/main/java/top/rootu/lampa/AndroidJS.kt
@@ -59,7 +59,6 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     fun storageChange(json: String?) {
         val hash = json.hashCode().toString()
         if (hash == lastEventHash) {
@@ -201,13 +200,11 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
 
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     fun appVersion(): String {
         return BuildConfig.VERSION_NAME + "-" + BuildConfig.VERSION_CODE
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     fun exit() {
         try {
             mainActivity.runOnUiThread { mainActivity.appExit() }
@@ -217,7 +214,6 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     @Throws(JSONException::class)
     fun openTorrentLink(url: String, jsonString: String): Boolean {
         val jsonData = if (jsonString == "\"\"") JSONObject() else JSONObject(jsonString)
@@ -271,7 +267,6 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     fun openYoutube(str: String) {
         val intent = Intent(
             Intent.ACTION_VIEW,
@@ -288,7 +283,6 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     fun openBrowser(url: String) {
         val intent = Intent(Intent.ACTION_VIEW, url.toUri())
         mainActivity.runOnUiThread {
@@ -305,7 +299,6 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     fun clearDefaultPlayer() {
         mainActivity.runOnUiThread {
             mainActivity.setPlayerPackage("", false)
@@ -315,7 +308,6 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     fun httpReq(str: String, returnI: Int) {
         debugLog(TAG, "httpReq JSON $str")
         try {
@@ -417,7 +409,6 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     fun getResp(str: String): String? {
         var string: String? = ""
         if (reqResponse.containsKey(str)) {
@@ -428,7 +419,6 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     fun openPlayer(link: String, jsonStr: String) {
         debugLog(TAG, "openPlayer: $link json:$jsonStr")
         val jsonObject = try {
@@ -455,19 +445,16 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     fun setProxyPAC(link: String): Boolean {
         return Http.setProxyPAC(link)
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     fun getProxyPAC(): String {
         return Http.getProxyPAC()
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     fun voiceStart() {
         // Голосовой ввод с последующей передачей результата через JS
         mainActivity.runOnUiThread {
@@ -487,7 +474,6 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
      * @param json The JSON string containing bookmarks.
      */
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     fun saveBookmarks(json: String?) {
         debugLog(TAG, "saveBookmarks fired!")
         CoroutineScope(Dispatchers.Default).launch {
@@ -505,7 +491,6 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     fun updateChannel(where: String?) {
         // https://github.com/yumata/lampa-source/blob/e5505b0e9cf5f95f8ec49bddbbb04086fccf26c8/src/app.js#L203
         if (where != null && isTvContentProviderAvailable) {
@@ -539,7 +524,6 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
     // https://stackoverflow.com/a/41560207
     // https://copyprogramming.com/howto/android-webview-savestate
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     @Synchronized
     fun dump() {
         check(!dumped) { "already dumped" }
@@ -555,7 +539,6 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     @Synchronized
     fun size(): Int {
         check(dumped) { "dump() first" }
@@ -563,7 +546,6 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     @Synchronized
     fun key(i: Int): String? {
         check(dumped) { "dump() first" }
@@ -571,7 +553,6 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     @Synchronized
     fun value(i: Int): String? {
         check(dumped) { "dump() first" }
@@ -579,14 +560,12 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     @Synchronized
     operator fun get(key: String?): String? {
         return store.getString(key, null)
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     @Synchronized
     operator fun set(key: String?, value: String?) {
         check(!dumped) { "already dumped" }
@@ -594,7 +573,6 @@ class AndroidJS(private val mainActivity: MainActivity, private val browser: Bro
     }
 
     @JavascriptInterface
-    @org.xwalk.core.JavascriptInterface
     @Synchronized
     fun clear() {
         store.edit { clear() }
